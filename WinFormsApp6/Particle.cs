0319c8ef761e7bae919dfc4580fe8fa9c8457fa8
Bogdanov_VP <vovabog071@gmail.com>
2024-05-27 07:57:25 +0800

    Дошел до "Выводим частицы"

---
+++ WinFormsApp6/Particle.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WinFormsApp6
+{
+	public class Particle
+	{
+		public int Radius; // радиус частицы
+		public float X; // X координата положения частицы в пространстве
+		public float Y; // Y координата положения частицы в пространстве
+
+		public float Direction; // направление движения
+		public float Speed; // скорость перемещения
+
+        public float Life; // запас здоровья частицы
+
+        public static Random rand = new Random();
+
+        public Particle()
+        {
+            Direction = rand.Next(360);
+            Speed = 1 + rand.Next(10);
+            Radius = 2 + rand.Next(10);
+            Life = 20 + rand.Next(100); // Добавили исходный запас здоровья от 20 до 120
+        }
+
+        public void Draw(Graphics g)
+        {
+            // создали кисть для рисования
+            var b = new SolidBrush(Color.Black);
+
+            // нарисовали залитый кружок радиусом Radius с центром в X, Y
+            g.FillEllipse(b, X - Radius, Y - Radius, Radius * 2, Radius * 2);
+
+            // удалили кисть из памяти, вообще сборщик мусора рано или поздно это сам сделает
+            // но документация рекомендует делать это самому
+            b.Dispose();
+        }
+    }
+}
