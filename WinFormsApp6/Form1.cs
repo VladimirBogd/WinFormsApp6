ba0700dc7f320dbbd046d0e0bcac5f20df12ba3c
Bogdanov_VP <vovabog071@gmail.com>
2024-05-27 08:14:57 +0800

    Дошёл до "Добавляем смену цвета"

--- WinFormsApp6/Form1.cs
+++ WinFormsApp6/Form1.cs
@@ -9,47 +9,62 @@ namespace WinFormsApp6
 		{
 			InitializeComponent();
 			picDisplay.Image = new Bitmap(picDisplay.Width, picDisplay.Height);
-
-			// генерирую 500 частиц
-			for (var i = 0; i < 500; ++i)
-			{
-				var particle = new Particle();
-				// переношу частицы в центр изображения
-				particle.X = picDisplay.Image.Width / 2;
-				particle.Y = picDisplay.Image.Height / 2;
-				// добавляю список
-				particles.Add(particle);
-			}
 		}
 
 		// добавил функцию обновления состояния системы
 		private void UpdateState()
 		{
-			foreach (var particle in particles)
-			{
-				particle.Life -= 1; // уменьшаю здоровье
-				// если здоровье кончилось
-				if (particle.Life < 0)
-				{
-					// восстанавливаю здоровье
-					particle.Life = 20 + Particle.rand.Next(100);
-					// перемещаю частицу в центр
-					particle.X = picDisplay.Image.Width / 2;
-					particle.Y = picDisplay.Image.Height / 2;
+            foreach (var particle in particles)
+            {
+                particle.Life -= 1;  // не трогаем
+                if (particle.Life < 0)
+                {
+                    // тоже не трогаем
+                    particle.Life = 20 + Particle.rand.Next(100);
+                    particle.X = MousePositionX;
+                    particle.Y = MousePositionY;
 
-                    particle.Direction = Particle.rand.Next(360);
-                    particle.Speed = 1 + Particle.rand.Next(10);
+                    /* ЭТО ДОБАВЛЯЮ, тут сброс состояния частицы */
+                    var direction = (double)Particle.rand.Next(360);
+                    var speed = 1 + Particle.rand.Next(10);
+
+                    particle.SpeedX = (float)(Math.Cos(direction / 180 * Math.PI) * speed);
+                    particle.SpeedY = -(float)(Math.Sin(direction / 180 * Math.PI) * speed);
+                    /* конец ЭТО ДОБАВЛЯЮ  */
+
+                    // это не трогаем
                     particle.Radius = 2 + Particle.rand.Next(10);
                 }
-				else
-				{
-					// а это наш старый код
-					var directionInRadians = particle.Direction / 180 * Math.PI;
-					particle.X += (float)(particle.Speed * Math.Cos(directionInRadians));
-					particle.Y -= (float)(particle.Speed * Math.Sin(directionInRadians));
-				}
-			}
-		}
+                else
+                {
+                    // и добавляем новый, собственно он даже проще становится, 
+                    // так как теперь мы храним вектор скорости в явном виде и его не надо пересчитывать
+                    particle.X += particle.SpeedX;
+                    particle.Y += particle.SpeedY;
+                }
+            }
+
+            // добавил генерацию частиц
+            // генерирую не более 10 штук за тик
+            for (var i = 0; i < 10; ++i)
+            {
+                if (particles.Count < 500) // пока частиц меньше 500 генерируем новые
+                {
+                    // а у тут уже наш новый класс используем
+                    var particle = new ParticleColorful();
+                    // ну и цвета меняем
+                    particle.FromColor = Color.Yellow;
+                    particle.ToColor = Color.FromArgb(0, Color.Magenta);
+                    particle.X = MousePositionX;
+                    particle.Y = MousePositionY;
+                    particles.Add(particle);
+                }
+                else
+                {
+                    break; // а если частиц уже 500 штук, то ничего не генерирую
+                }
+            }
+        }
 
 		// функция рендеринга
 		private void Render(Graphics g)
@@ -68,11 +83,22 @@ namespace WinFormsApp6
 
 			using (var g = Graphics.FromImage(picDisplay.Image))
 			{
-				g.Clear(Color.White);
-				Render(g); // рендерим систему
+                g.Clear(Color.Black); // А ЕЩЕ ЧЕРНЫЙ ФОН СДЕЛАЮ
+                Render(g); // рендерим систему
 			}
 
 			picDisplay.Invalidate();
 		}
-	}
+
+        // добавляем переменные для хранения положения мыши
+        private int MousePositionX = 0;
+        private int MousePositionY = 0;
+
+        private void picDisplay_MouseMove(object sender, MouseEventArgs e)
+        {
+            // в обработчике заносим положение мыши в переменные для хранения положения мыши
+            MousePositionX = e.X;
+            MousePositionY = e.Y;
+        }
+    }
 }
